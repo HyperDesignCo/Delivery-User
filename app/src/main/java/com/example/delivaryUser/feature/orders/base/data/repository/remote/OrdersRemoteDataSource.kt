b3983e97c50d3d1d5510511fb4905503287b8b35
Rodinamomen <Rodinamobark3@gmail.com>
2026-01-28 15:50:30 +0200

    wip: add remote data sources

--- app/src/main/java/com/example/delivaryUser/feature/orders/base/data/repository/remote/OrdersRemoteDataSource.kt
+++ app/src/main/java/com/example/delivaryUser/feature/orders/base/data/repository/remote/OrdersRemoteDataSource.kt
@@ -1,11 +1,11 @@
 package com.example.delivaryUser.feature.orders.base.data.repository.remote
 
 import com.example.delivaryUser.common.domain.remote.IRemoteDataSourceProvider
-import com.example.delivaryUser.feature.orders.base.data.models.dto.OrderDetailsDto
+import com.example.delivaryUser.feature.orders.base.data.models.dto.OrderResponseDto
 import com.example.delivaryUser.feature.orders.base.data.models.dto.OrdersDto
 import com.example.delivaryUser.feature.orders.base.domain.repository.remote.IOrdersRemoteDataSource
 
-class OrdersRemoteDataSource(val provider: IRemoteDataSourceProvider) : IOrdersRemoteDataSource {
+class OrdersRemoteDataSource(private val provider: IRemoteDataSourceProvider) : IOrdersRemoteDataSource {
     override suspend fun getOrders(token: String): OrdersDto = provider.get(
         headers = mapOf("Authorization" to "Bearer $token"),
         endpoint = ORDERS_ENDPOINT,
@@ -15,11 +15,11 @@ class OrdersRemoteDataSource(val provider: IRemoteDataSourceProvider) : IOrdersR
     override suspend fun getOrderById(
         token: String,
         id: Int,
-    ): OrderDetailsDto = provider.get(
+    ): OrderResponseDto = provider.get(
         headers = mapOf("Authorization" to "Bearer $token"),
         params = mapOf("id" to id),
         endpoint = ORDER_DETAILS,
-        serializer = OrderDetailsDto.serializer(),
+        serializer = OrderResponseDto.serializer(),
     )
 
     companion object {
