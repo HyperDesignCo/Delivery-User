f3799efdfcba63fefe252ed45477b9a4cf7d0d37
Rodinamomen <Rodinamobark3@gmail.com>
2026-01-28 15:55:22 +0200

    wip: add required graph

---
+++ app/src/main/java/com/example/delivaryUser/common/ui/navigation/IOrderGraph.kt
@@ -0,0 +1,32 @@
+package com.example.delivaryUser.common.ui.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.example.delivaryUser.feature.addresslist.ui.view.AddressListScreen
+import com.example.delivaryUser.feature.pointtopoint.ui.components.AddressType
+import com.example.delivaryUser.feature.pointtopoint.ui.view.PointToPointScreen
+import kotlinx.serialization.Serializable
+
+sealed interface IOrderGraph : IDestination {
+
+    @Serializable
+    data object RootGraph : IOrderGraph
+
+    @Serializable
+    data class AddressList(val id: Int? = null, val addressType: AddressType) : IOrderGraph
+
+    @Serializable
+    data object PointToPoint : IOrderGraph
+}
+
+fun NavGraphBuilder.buildNavOrderGraph() {
+    navigation<IOrderGraph.RootGraph>(startDestination = IOrderGraph.PointToPoint) {
+        composable<IOrderGraph.PointToPoint> {
+            PointToPointScreen()
+        }
+        composable<IOrderGraph.AddressList> {
+            AddressListScreen()
+        }
+    }
+}
