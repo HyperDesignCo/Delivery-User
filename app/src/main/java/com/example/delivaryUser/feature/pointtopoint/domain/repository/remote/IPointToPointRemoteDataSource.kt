a5e23e5a55ea8434fcea841570d674990a72e195
Rodinamomen <Rodinamobark3@gmail.com>
2026-02-08 17:51:05 +0200

    wip: add required Repository and DataSource interfaces

--- app/src/main/java/com/example/delivaryUser/feature/pointtopoint/domain/repository/remote/IPointToPointRemoteDataSource.kt
+++ app/src/main/java/com/example/delivaryUser/feature/pointtopoint/domain/repository/remote/IPointToPointRemoteDataSource.kt
@@ -1,7 +1,6 @@
 package com.example.delivaryUser.feature.pointtopoint.domain.repository.remote
 
-import com.example.delivaryUser.feature.orders.base.data.models.dto.CheckOutOrderResponseDto
-import com.example.delivaryUser.feature.orders.base.data.models.dto.OrderResponseDto
+import com.example.delivaryUser.feature.orders.base.data.models.dto.NewOrderResponseDto
 import com.example.delivaryUser.feature.pointtopoint.data.models.dto.DeliveryCostResponseDto
 import com.example.delivaryUser.feature.pointtopoint.data.models.dto.OrderPurposesResponseDto
 import com.example.delivaryUser.feature.pointtopoint.data.models.request.DeliveryCostRequest
@@ -10,5 +9,5 @@ import com.example.delivaryUser.feature.pointtopoint.data.models.request.PointTo
 interface IPointToPointRemoteDataSource {
     suspend fun getOrderPurposes(token: String): OrderPurposesResponseDto
     suspend fun getDeliveryCost(token: String, request: DeliveryCostRequest): DeliveryCostResponseDto
-    suspend fun addNewOrder(token: String, request: PointToPointRequest): CheckOutOrderResponseDto
+    suspend fun addNewOrder(token: String, request: PointToPointRequest): NewOrderResponseDto
 }
