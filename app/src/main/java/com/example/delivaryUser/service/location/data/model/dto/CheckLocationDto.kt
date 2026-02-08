fa6b63fb765ce7c07cc1f72ccc357d900d1bff80
Rodinamomen <Rodinamobark3@gmail.com>
2026-02-08 17:45:44 +0200

    wip: add required Data Dto models

--- app/src/main/java/com/example/delivaryUser/service/location/data/model/dto/CheckLocationDto.kt
+++ app/src/main/java/com/example/delivaryUser/service/location/data/model/dto/CheckLocationDto.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CheckLocationDto(
-    @SerialName("message") val message: String,
+    @SerialName("message") val message: String? = null,
     @SerialName("data") val data: LocationDto? = null,
 )
 
