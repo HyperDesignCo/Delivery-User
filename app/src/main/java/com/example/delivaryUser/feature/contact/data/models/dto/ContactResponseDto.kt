8850f6122f559cd927c973fb64faa38b15bc0729
Rodinamomen <Rodinamobark3@gmail.com>
2026-02-19 11:03:04 +0200

    wip: add dto model.

---
+++ app/src/main/java/com/example/delivaryUser/feature/contact/data/models/dto/ContactResponseDto.kt
@@ -0,0 +1,12 @@
+package com.example.delivaryUser.feature.contact.data.models.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ContactResponseDto(
+    @SerialName("Contactus")
+    val contacts: ContactDto?=null,
+    @SerialName("message")
+    val message: String?=null,
+)
