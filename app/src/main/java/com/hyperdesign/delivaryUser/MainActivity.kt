a1f2895dcf255bb270ddba2b2bc0334810c4fe8d
Rodinamomen <Rodinamobark3@gmail.com>
2026-02-23 13:44:39 +0200

    wip: add Notification permission.

--- app/src/main/java/com/hyperdesign/delivaryUser/MainActivity.kt
+++ app/src/main/java/com/hyperdesign/delivaryUser/MainActivity.kt
@@ -1,19 +1,33 @@
 package com.hyperdesign.delivaryUser
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.app.ActivityCompat
 import com.hyperdesign.delivaryUser.common.ui.theme.DelivaryUserTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestNotificationPermission()
         enableEdgeToEdge()
         setContent {
             DelivaryUserTheme {
-               DelivaryUserApp()
+                DelivaryUserApp()
             }
+        }
+    }
+
+    private fun requestNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                0
+            )
         }
     }
 }
