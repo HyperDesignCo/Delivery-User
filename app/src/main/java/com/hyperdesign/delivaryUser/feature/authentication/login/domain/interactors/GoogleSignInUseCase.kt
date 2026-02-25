c7adca8ff57aea3a2a57c50edaad4a765ef3c095
Rodinamomen <Rodinamobark3@gmail.com>
2026-02-25 11:23:14 +0200

    wip: Add Google sign in usecase

---
+++ app/src/main/java/com/hyperdesign/delivaryUser/feature/authentication/login/domain/interactors/GoogleSignInUseCase.kt
@@ -0,0 +1,31 @@
+package com.hyperdesign.delivaryUser.feature.authentication.login.domain.interactors
+
+import android.content.Context
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.hyperdesign.delivaryUser.common.domain.Resource
+import com.hyperdesign.delivaryUser.common.domain.usecase.BaseUseCase
+import com.hyperdesign.delivaryUser.feature.authentication.login.domain.models.GoogleSignInResult
+import kotlinx.coroutines.flow.Flow
+
+class GoogleSignInUseCase(private val context: Context) : BaseUseCase<Flow<Resource<GoogleSignInResult>>, Int>() {
+    override suspend fun invoke(body: Int): Flow<Resource<GoogleSignInResult>> = flowExecute {
+        val credentialManager = CredentialManager.create(context)
+        val googleIdOption = GetGoogleIdOption.Builder()
+            .setFilterByAuthorizedAccounts(false)
+            .setServerClientId(context.getString(body))
+            .build()
+        val request = GetCredentialRequest.Builder()
+            .addCredentialOption(googleIdOption)
+            .build()
+        val result = credentialManager.getCredential(context, request)
+        val credential = GoogleIdTokenCredential.createFrom(result.credential.data)
+        GoogleSignInResult(
+            email = credential.id,
+            name = credential.displayName.orEmpty(),
+            socialId = credential.id
+        )
+    }
+}
