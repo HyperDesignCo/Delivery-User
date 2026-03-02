7ab602abf210bc865c6074db9cb2efd4566a8bcc
Rodinamomen <Rodinamobark3@gmail.com>
2026-03-02 12:33:00 +0200

    wip: Add Google sign in manager

--- app/src/main/java/com/hyperdesign/delivaryUser/service/googlesignin/GoogleSignInManager.kt
+++ app/src/main/java/com/hyperdesign/delivaryUser/service/googlesignin/GoogleSignInManager.kt
@@ -1,28 +1,25 @@
-package com.hyperdesign.delivaryUser.feature.authentication.login.domain.interactors
+package com.hyperdesign.delivaryUser.service.googlesignin
 
 import android.content.Context
 import androidx.credentials.CredentialManager
 import androidx.credentials.GetCredentialRequest
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
-import com.hyperdesign.delivaryUser.common.domain.Resource
-import com.hyperdesign.delivaryUser.common.domain.usecase.BaseUseCase
+import com.hyperdesign.delivaryUser.R
 import com.hyperdesign.delivaryUser.feature.authentication.login.domain.models.GoogleSignInResult
-import kotlinx.coroutines.flow.Flow
 
-class GoogleSignInUseCase(private val context: Context) : BaseUseCase<Flow<Resource<GoogleSignInResult>>, Int>() {
-    override suspend fun invoke(body: Int): Flow<Resource<GoogleSignInResult>> = flowExecute {
+class GoogleSignInManager(private val context: Context) {
+    suspend fun signIn(): GoogleSignInResult {
         val credentialManager = CredentialManager.create(context)
-        val googleIdOption = GetGoogleIdOption.Builder()
-            .setFilterByAuthorizedAccounts(false)
-            .setServerClientId(context.getString(body))
+        val googleSignInOption = GetSignInWithGoogleOption
+            .Builder(serverClientId = context.getString(R.string.default_web_client_id))
             .build()
         val request = GetCredentialRequest.Builder()
-            .addCredentialOption(googleIdOption)
+            .addCredentialOption(googleSignInOption)
             .build()
         val result = credentialManager.getCredential(context, request)
         val credential = GoogleIdTokenCredential.createFrom(result.credential.data)
-        GoogleSignInResult(
+        return GoogleSignInResult(
             email = credential.email.orEmpty(),
             name = credential.displayName.orEmpty(),
             socialId = credential.uniqueId
